e13148a07df820c87baf3565df29a3b52b5cdfc5
mamitiana384 <se20190026@gmail.com>
2025-02-14 15:33:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import streamlit as st
 
-# Si l'utilisateur est connecté, on charge l'application principale
-if "authenticated" in st.session_state and st.session_state["authenticated"]:
-    st.switch_page("pages/app7.py")  # Vers app7.py dans le dossier 'pages'
-else:
-    st.switch_page("pages/login.py")  # Vers login.py dans le dossier 'pages'
+# Vérifier si l'utilisateur est authentifié
+def check_authentication():
+    if "authenticated" not in st.session_state or not st.session_state["authenticated"]:
+        st.session_state["authenticated"] = False
+
+# Page principale
+def main():
+    check_authentication()
+
+    if st.session_state["authenticated"]:
+        st.switch_page("pages/app7.py")  # Aller vers app7.py si l'utilisateur est authentifié
+    else:
+        st.switch_page("pages/login.py")  # Sinon, aller vers login.py
+
+if __name__ == "__main__":
+    main()
