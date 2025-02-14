8799ca0d536250e320699dd82adf7b462fa008c2
mamitiana384 <se20190026@gmail.com>
2025-02-14 15:47:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import streamlit as st
+from session import load_session_state  # Importation de la fonction pour charger l'état de la session
 
 # Vérifier si l'utilisateur est authentifié
 def check_authentication():
-    if "authenticated" not in st.session_state or not st.session_state["authenticated"]:
-        st.session_state["authenticated"] = False
+    session_state = load_session_state()
+    if "authenticated" not in session_state or not session_state["authenticated"]:
+        session_state["authenticated"] = False
+    return session_state
 
 # Page principale
 def main():
-    check_authentication()
+    session_state = check_authentication()  # Vérifie si l'utilisateur est authentifié
 
-    if st.session_state["authenticated"]:
+    if session_state["authenticated"]:
         st.switch_page("pages/app7.py")  # Aller vers app7.py si l'utilisateur est authentifié
     else:
         st.switch_page("pages/login.py")  # Sinon, aller vers login.py
